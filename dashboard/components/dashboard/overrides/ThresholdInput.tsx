310495db3c58eee418d6f89a09f6683eb16a48e8
Ayush <sudhirsoni9889@gmail.com>
2026-02-24 11:44:30 +0530

    fix the ui bugs

--- dashboard/components/dashboard/overrides/ThresholdInput.tsx
+++ dashboard/components/dashboard/overrides/ThresholdInput.tsx
@@ -47,7 +47,7 @@ export function ThresholdInput({
             }`}
           >
             <div
-              className={`absolute top-0.5 left-0.5 w-4 h-4 rounded-full bg-white transition-transform ${
+              className={`absolute top-0.5  w-4 h-4 rounded-full bg-white transition-transform ${
                 enabled ? "translate-x-4" : ""
               }`}
             />
